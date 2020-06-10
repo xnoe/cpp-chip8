4edb7018ab190fb02209993cd097a3f6585d8863
Xnoe <xnoe@xnoe.moe>
2020-06-10 17:41:28 +0100

    No longer cheats and stores most stuff within RAM

--- main.cpp
+++ main.cpp
@@ -9,24 +9,25 @@ int main(int argc, char** argv) {
   }
 
   unsigned char ROM[0x1000] = {0};
-  unsigned char Vx[0xF];
-  unsigned short I(0);
+  unsigned char* Vx = &ROM[0xEA0];
+  unsigned short* I = (unsigned short*)ROM+0xEB0;
+  unsigned short* PC = (unsigned short*)ROM+0xEB2;
+
   unsigned short INST;
-  unsigned short PC(0);
 
   std::ifstream romFile (argv[1]);
 
-  while (!romFile.eof() && PC<0x1000)
-    ROM[PC++] = romFile.get();
-  ROM[--PC] = 0;
+  int count(0);
+  while (!romFile.eof() && count<0x1000)
+    ROM[count++] = romFile.get();
+  ROM[--count] = 0;
   romFile.close();
-  PC = 0;
 
   std::stack<unsigned short> stack;
 
   bool running = true;
-  while (running && PC<0x1000) {
-    INST = (ROM[PC]<<8)|(ROM[PC+1]);
+  while (running && *PC<0x1000) {
+    INST = (ROM[*PC]<<8)|(ROM[*PC+1]);
     unsigned short NNN = INST&4095;
     unsigned char NN = (char)INST;
     unsigned char N = (char)INST&15;
@@ -38,24 +39,24 @@ int main(int argc, char** argv) {
         if (!NNN) running = false;
         if (NNN==0x0E0) break;
         if (NNN==0x0EE) {
-          PC = stack.top();stack.pop();
+          *PC = stack.top();stack.pop();
         }
       }
       case (1):
-        PC = NNN-2;
+        *PC = NNN-2;
         break;
       case (2):
-        stack.push(PC);PC = NNN;break;
+        stack.push(*PC);*PC = NNN;break;
       case (3):
         if (Vx[X]==NN)
-          PC+=2;
+          *PC+=2;
         break;
       case (4):
         if (Vx[X]!=NN)
-          PC+=2;
+          *PC+=2;
         break;
       case (5):
-        if (Vx[X]==Vx[Y]) PC+=2;
+        if (Vx[X]==Vx[Y]) *PC+=2;
         break;
       case (6):
         Vx[X] = NN;
@@ -89,13 +90,13 @@ int main(int argc, char** argv) {
         }
         break;
       case (9):
-        if (Vx[X]!=Vx[Y]) PC+=2;
+        if (Vx[X]!=Vx[Y]) *PC+=2;
         break;
       case (10):
-        I = NNN;
+        *I = NNN;
         break;
       case (11):
-        PC = *Vx+NNN;
+        *PC = *Vx+NNN;
         break;
       case (12):
         // Not Implemented;
@@ -103,24 +104,25 @@ int main(int argc, char** argv) {
       case (13):
         // This would usually be DXYN for draw
         // But here it is used to dump memory location at I
-        std::cout << (char)ROM[I];
+        std::cout << (char)ROM[*I];
         break;
       case (14):
         // Not Implemented;
       case (15):
         // Partially Implemented;
-        if (NN == 0x1E) I += Vx[X];
+        if (NN == 0x1E) *I += Vx[X];
         else if (NN == 0x55) {
-          unsigned short tmp(I);
+          unsigned short tmp(*I);
           for (int i(0);i<=X;i++)
             ROM[tmp++]=Vx[i];
         } else if (NN == 0x65) {
-          unsigned short tmp(I);
+          unsigned short tmp(*I);
           for (int i(0);i<=X;i++)
             Vx[i]=ROM[tmp++];
         }
     }
-    PC+=2;
+    *PC+=2;
   }
+  std::cout << std::flush;
   return 0;
 }
